bdd4246102e4e3ccb526516e11ca22a18cc1186b
Ahmed Khallaf <[ahmedkhallaf2098@gmail.com]>
2023-12-01 10:27:11 +0200

    Dart Frog: GET data from database

---
+++ api/lib/src/repositories/message_repository.dart
@@ -0,0 +1,32 @@
+
+import 'package:supabase/supabase.dart';
+
+class MessageRepository {
+  final SupabaseClient dbClient;
+
+  MessageRepository({
+    required this.dbClient,
+  });
+
+
+  Future<Map<String, dynamic>> createMessage(Map<String, dynamic> data) async {
+    try {
+      return await dbClient.from('messages').insert(data).select().single();
+    } catch (err) {
+      throw Exception(err);
+    }
+  }
+
+  Future<List<Map<String, dynamic>>> fetchMessages(String chatRoomId) async {
+    try {
+      final messages = await dbClient
+          .from('messages')
+          .select<PostgrestList>()
+          .eq('chat_room_id', chatRoomId);
+
+      return messages;
+    } catch (err) {
+      throw Exception(err);
+    }
+  }
+}
